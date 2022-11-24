12c4d09ea8370608ad7e49b5a20506355cc3c7ca
Daniel Marques <daniel.brz2009@gmail.com>
2022-11-24 01:49:15 -0300

    wip

--- apps/web/src/pages/api/auth/[...nextauth].ts
+++ apps/web/src/pages/api/auth/[...nextauth].ts
@@ -1,31 +1,38 @@
 import NextAuth, { type NextAuthOptions } from 'next-auth'
 import DiscordProvider from 'next-auth/providers/discord'
-import GoogleProvider from 'next-auth/providers/github'
-// Prisma adapter for NextAuth, optional and can be removed
+import GithubProvider from 'next-auth/providers/github'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 
-import { env } from '../../../env/server.mjs'
-import { prisma } from '../../../server/db/client'
+import { env } from 'env/server.mjs'
+import { prisma } from 'server/db/client'
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
   callbacks: {
     session({ session, user }) {
+      console.log({ session, user })
+
       if (session.user) {
         session.user.id = user.id
       }
       return session
     },
   },
+
   adapter: PrismaAdapter(prisma),
+
+  theme: {
+    colorScheme: 'auto',
+  },
+
   providers: [
-    // GoogleProvider({
-    //   clientId: env.DISCORD_CLIENT_ID,
-    //   clientSecret: env.DISCORD_CLIENT_SECRET,
-    // }),
     DiscordProvider({
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
+    }),
+    GithubProvider({
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
     }),
   ],
 }
